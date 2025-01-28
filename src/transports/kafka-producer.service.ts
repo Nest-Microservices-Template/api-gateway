9c34e17a11f821cf0f66a74c859a0b9174f71cab
Yesfer <yesfer.bany@gmail.com>
2025-01-28 12:36:49 -0600

    feat: message sending class is added

---
+++ src/transports/kafka-producer.service.ts
@@ -0,0 +1,35 @@
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable()
+export class KafkaProducerService implements OnModuleInit {
+  constructor(@Inject('KAFKA_CLIENT') private readonly client: ClientKafka) {}
+
+  async onModuleInit() {
+    const replyTopics = [
+      'find_all_products',
+      'create_product',
+      'find_one_product',
+      'createOrder',
+      'findOneOrder',
+      'findAllOrders',
+      'changeOrderStatusTopic',
+      'validate_products',
+    ];
+
+    replyTopics.forEach((topic) => {
+      this.client.subscribeToResponseOf(`${topic}`);
+    });
+
+    await this.client.connect();
+  }
+
+  async sendMessage(topic: string, message: any) {
+    return this.client.emit(topic, JSON.stringify(message));
+  }
+
+  async sendAndReceive<T>(topic: string, message: any): Promise<T> {
+    return lastValueFrom(this.client.send(topic, JSON.stringify(message)));
+  }
+}
